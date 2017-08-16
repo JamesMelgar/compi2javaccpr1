0c8af35e3d01fca56aa5e88a8c2aee70e55e114c
LAPTOP-6QF2THB4\James_PC <james.melgar@gmail.com>
2017-08-15 21:39:34 -0600

    version 1

---
+++ pr1Compilarodores2/src/pr1compilarodores2/Pr1Compilarodores2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pr1compilarodores2;
+
+public class Pr1Compilarodores2 {
+
+    public static void main(String[] args) {
+        principal2 pr = new principal2();
+        pr.setVisible(true);
+                
+    }
+    
+}
